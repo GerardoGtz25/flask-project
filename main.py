29c534290fd7ab700e9310bf9466e589ce957eec
Gerardo <ge.gut5@gmail.com>
2020-02-20 23:42:56 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+todos = ['Comprar cafe', 'Enviar solicitude de compra', 'Lavar ropa']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip =session.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos
+    }
+
+    return render_template('hello.html', **context)
+
+@app.route('/test')
+def test():
+   return 'hola'
